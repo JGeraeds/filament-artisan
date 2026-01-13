1e1a42aaea69aaae1db0646091eff6598af46d90
Joey Geraeds <joeygeraeds@vostermans.com>
2026-01-13 15:38:41 +0100

    only load the style when needed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,8 @@
-<x-filament-panels::page>
-    {{ $this->table }}
-</x-filament-panels::page>
+<div
+        x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-artisan', package: 'filament-artisan'))]"
+>
+    <x-filament-panels::page>
+        {{ $this->table }}
+    </x-filament-panels::page>
+</div>
+
